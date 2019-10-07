a19cc5a9f4a4c83379467a59de63c37f88cff903
ilgoncalves <lima-igor@hotmail.com>
2019-10-06 22:09:05 -0300

    create dashboard model and controller

--- back-end/src/controllers/SpotController.js
+++ back-end/src/controllers/SpotController.js
@@ -1,8 +1,33 @@
+const User = require('../models/User')
 const Spot = require('../models/Spot')
 module.exports = {
+    async index(req, res) {
+        const { tech } = req.query;
+        const spots = await Spot.find({ techs: tech });
+
+        return res.json(spots)
+    },
+
     async store(req, res) {
-        console.log(req.body);
-        console.log(req.file);
-        return res.json({ "ok": true })
+        const { filename } = req.file
+        const { company, price, techs } = req.body
+        const { user_id } = req.headers
+
+        const user = await User.findById(user_id)
+        if (!user) {
+            return res.status(400).json({ error: 'Usuario não existe' })
+        }
+
+        const spot = await Spot.create({
+            image: filename,
+            company: company,
+            price: price,
+            techs: techs.split(',').map(item => item.trim()),
+            user: user_id
+        });
+
+
+
+        return res.json(spot)
     }
 }
--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -5,15 +5,20 @@ const uploadConfig = require('./config/upload');
 
 const SessionController = require('./controllers/SessionController')
 const SpotController = require('./controllers/SpotController')
+const DashboardController = require('./controllers/DashboardControlles')
 
 const routes = express.Router();
 
 const upload = multer(uploadConfig)
 
 
-
+//session
 routes.post('/sessions', SessionController.store)
+//spot
 routes.post('/spots', upload.single('image'), SpotController.store)
+routes.get('/spots', SpotController.index)
+//dashboard
+routes.get('/getSpotsOfUser', DashboardController.show)
 
 
 module.exports = routes; 
