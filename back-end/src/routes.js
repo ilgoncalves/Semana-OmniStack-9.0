526bb5458a0d0663c6a38b9479243da36229fdff
ilgoncalves <lima-igor@hotmail.com>
2019-10-06 22:28:06 -0300

    create the controller and model of booking

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -5,7 +5,8 @@ const uploadConfig = require('./config/upload');
 
 const SessionController = require('./controllers/SessionController')
 const SpotController = require('./controllers/SpotController')
-const DashboardController = require('./controllers/DashboardControlles')
+const DashboardController = require('./controllers/DashboardController')
+const BookingController = require('./controllers/BookingController')
 
 const routes = express.Router();
 
@@ -17,6 +18,7 @@ routes.post('/sessions', SessionController.store)
 //spot
 routes.post('/spots', upload.single('image'), SpotController.store)
 routes.get('/spots', SpotController.index)
+routes.get('/spots/:spot_id/bookings', BookingController.store)
 //dashboard
 routes.get('/getSpotsOfUser', DashboardController.show)
 
